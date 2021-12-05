b4058f6dc45053537580e3cdefc63dbe1d41a94b
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-05 12:57:12 +0400

    declare trimNumber function

--- task3.js
+++ task3.js
@@ -1,6 +1,7 @@
 // Given a phone number. Write a function to clean it up, so it is valid. The rules are as follows:
 
 // If the phone number is 10 digits assume that it is good
+
 // If the phone number consists of 11 symbols and the first one is + and others are numbers, then trim + and return remaining 10 digits.
 
 // If the phone number is less than 10 digits assume that it is a bad number
@@ -8,7 +9,7 @@
 // If the phone number contains + symbol more than one, consider it as a bad number.
 // If the phone number contains + symbol not as the first character, consider it as a bad number.
 
-let number = '142345678';
+let number = '+14234567811';
 
 function badNumber(num) {
   if (num.length > 10 || num.length < 10) console.log('Bad Number');
@@ -19,4 +20,8 @@ function goodNumber(num) {
   if (num.length === 10) console.log('Good number');
 }
 
-goodNumber(number);
+function trimNumber(num) {
+  if (num.length === 11 && num[0] === '+') num = num.substring(1);
+  console.log(num);
+}
+trimNumber(number);
