97cd368c5b287e2a9ffb5c6eaf1d7bb403cf8656
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-05 11:52:41 +0400

    add task2.js description

---
+++ task2.js
@@ -0,0 +1,2 @@
+// Write a function that calculates sum, difference, multiplication and division between given array elements depending on passed operation symbol. Write appropriate function for each operation.
+'use strict';
