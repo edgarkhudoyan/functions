2878696998b5ed83f022c0ca0e6c4eea0adcb00c
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-05 14:21:59 +0400

    trying to check with function

--- task6.js
+++ task6.js
@@ -1,1 +1,10 @@
 // Write a function which receives two strings and removes appearances of the second string from the first one.
+
+let str1 = 'This is some text';
+let str2 = 'is';
+
+function checkInclude(where, what) {
+  if (where.includes(what)) return where.replace(what, '');
+}
+
+console.log(checkInclude('This is some text', 'is'));
