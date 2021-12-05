ba740896425145ddd2f648e0a6ad43e2e5bf17f7
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-05 11:47:17 +0400

    declare isEven function

--- task1.js
+++ task1.js
@@ -1,3 +1,10 @@
 // Given an array of numbers. Write a function to separate odd and even numbers in different arrays.
 'use strict';
 let arr = [45, 12, 3, 6, 17, 0];
+
+function isEven() {
+  const evens = arr.filter((e) => {
+    return e % 2 === 0;
+  });
+  console.log(evens);
+}
