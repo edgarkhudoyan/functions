e193cea992f679bc188af05e813a74da6f78d3ea
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-05 11:54:45 +0400

    declare sum function

--- task2.js
+++ task2.js
@@ -1,2 +1,10 @@
 // Write a function that calculates sum, difference, multiplication and division between given array elements depending on passed operation symbol. Write appropriate function for each operation.
 'use strict';
+let arr = [28, 7, 2];
+
+function sum(item) {
+  const totalNum = item.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+  console.log(totalNum);
+}
